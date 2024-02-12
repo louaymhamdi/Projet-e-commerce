728aabe8c73567ab5f16c2ade4802641821bdfcc
louaymhamdi <80352602+louaymhamdi@users.noreply.github.com>
2024-02-12 21:07:08 +0100

    script page checkout fonctionne

--- project e-comm/assets/js/script.js
+++ project e-comm/assets/js/script.js
@@ -25,16 +25,3 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 200 ? header.classList.add("active")
     : header.classList.remove("active");
 })
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('./header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header-placeholder').innerHTML = data;
-    });
-
-  fetch('./footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer-placeholder').innerHTML = data;
-    });
-});
